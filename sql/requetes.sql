824acfb501913242e612a3e3738595d067319f9d
Max FOUQUET <fouquet.max@gmail.com>
2021-03-13 22:12:15 +0100

    Ajout requête

--- sql/requetes.sql
+++ sql/requetes.sql
@@ -8,5 +8,11 @@ JOIN application ON application.idApp = contenir.idApp
 WHERE application.nomApp = "Pégase"
 ORDER BY severite DESC;
 
+-- Afficher le nombre de vulnérabilités par application
+SELECT application.nomApp as "Application", count(*) as "Nombre de vulnérabilités" FROM vulnerabilite
+RIGHT JOIN contenir ON vulnerabilite.idVuln = contenir.idVuln
+RIGHT JOIN application ON application.idApp = contenir.idApp
+GROUP BY application.nomApp;
+
 -- Afficher le détail de la vulnérabilité SQLi
 SELECT * FROM vulnerabilite WHERE idVuln = 3;
