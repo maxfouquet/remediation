5f0664468c3ac71da94c8a521edffd5eb68f1574
Max FOUQUET <fouquet.max@gmail.com>
2021-03-14 12:05:29 +0100

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3ec5a8e80253afdd3dd6c3b9539ac2de9a0e27c8',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3ec5a8e80253afdd3dd6c3b9539ac2de9a0e27c8',
+    ),
+  ),
+);
